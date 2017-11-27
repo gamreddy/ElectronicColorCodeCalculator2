62a66105fa21ee404c35b7a4938c176e3f338e81
gamreddy <gamreddy@yahoo.com>
2017-11-27 04:02:32 -0600

    CalculatorWebApplication
    Web interface for the Calculator

---
+++ CalculatorWebApplication/CalculatorWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Calculator;
+
+namespace CalculatorWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Calculator()
+        {
+            ViewBag.Message = "Electronic Color Code Calculator";
+            ColorCodes colorCodes = new ColorCodes();
+
+            Models.CalculatorViewModel viewModel = new Models.CalculatorViewModel();
+            viewModel.ColorCodes = colorCodes.getColorCodes().Values;
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Calculator(Models.CalculatorViewModel viewModel)
+        {
+
+            ViewBag.Message = "Electronic Color Code Calculator";
+            ColorCodes colorCodes = new ColorCodes();
+            viewModel.ColorCodes = colorCodes.getColorCodes().Values;
+
+            if (!ModelState.IsValid)
+            {
+                return View(viewModel);
+            }
+            else
+            {
+                IOhmValueCalculator calculator = new OhmValueCalcuator(new ColorCodes());
+                OhmRange range = calculator.CalculateOhmValueRange(viewModel.SelectedAColor, viewModel.SelectedBColor, viewModel.SelectedCColor, viewModel.SelectedDColor);
+
+                viewModel.OhmValueMin = range.MinValue;
+                viewModel.OhmValueMax = range.MaxValue;
+
+                return View(viewModel);
+            }
+        }
+    }
+}
---
+++ CalculatorWebApplication/CalculatorWebApplication/Models/CalculatorViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+using Calculator;
+
+namespace CalculatorWebApplication.Models
+{
+    public class CalculatorViewModel
+    {
+
+        [Required]
+        [Display(Name = "Band A Color")]
+        public string SelectedAColor { get; set; }
+
+        [Required]
+        [Display(Name = "Band B Color")]
+        public string SelectedBColor { get; set; }
+
+        [Required]
+        [Display(Name = "Band C Color")]
+        public string SelectedCColor { get; set; }
+
+        [Display(Name = "Band D Color")]
+        public string SelectedDColor { get; set; }
+
+        public IEnumerable<SelectListItem> SignificantColors
+        {
+            get
+            {
+                return new SelectList(ColorCodes.Where(x => x.SignificantFigures.HasValue), "Code", "Name");
+            }
+        }
+
+        public IEnumerable<SelectListItem> Colors
+        {
+            get
+            {
+                return new SelectList(ColorCodes, "Code", "Name");
+            }
+        }
+
+        public IEnumerable<ColorCode> ColorCodes
+        {
+            get;set;
+        }
+
+        public double OhmValueMin { get; set; }
+        public double OhmValueMax { get; set; }
+
+    }
+}
